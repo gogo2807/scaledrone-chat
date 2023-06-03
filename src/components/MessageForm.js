05424cf40fd820c594cda7874f50807ae6bdd9c4
gogo2807 <60366714+gogo2807@users.noreply.github.com>
2023-06-03 11:55:16 +0200

    Update MessageForm.js
    Dodana useEffect hookica

--- src/components/MessageForm.js
+++ src/components/MessageForm.js
@@ -1,25 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import "../styles/MessageForm.css";
 
 function MessageForm({ onSubmit }) {
   const [message, setMessage] = useState("");
-  const onChange = (event) => setMessage(event.target.value);
+  const inputRef = useRef(null);
+  const onChange = (event) => {
+    setMessage(event.target.value);
+  };
+
   const handleSubmit = () => {
     onSubmit(message);
     setMessage("");
   };
+
   const handleKeyDown = (event) => {
     if (event.key === "Enter") {
       handleSubmit();
     }
   };
 
+  useEffect(() => {
+    const input = inputRef.current;
+    if (input) {
+      input.style.height = "auto";
+      input.style.height = `${input.scrollHeight}px`;
+    }
+  }, [message]);
+
   return (
     <>
-      <input
+      <textarea
+        ref={inputRef}
         className="message-form__input"
         placeholder="Unesi poruku..."
-        type="text"
         value={message}
         onChange={onChange}
         onKeyDown={handleKeyDown}
