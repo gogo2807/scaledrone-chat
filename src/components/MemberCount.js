ac086c19ba2f0c716cef6e8b1c85c2582c2797b5
gogo2807 <60366714+gogo2807@users.noreply.github.com>
2023-06-03 14:17:12 +0200

    Update MemberCount.js
    Preimenovana klasa

--- src/components/MemberCount.js
+++ src/components/MemberCount.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function MemberCount({ count }) {
-  return <div className="member-count">{count} korisnika u sobi:</div>;
+  return <div className="members-count">{count} korisnika u sobi:</div>;
 }
 
 export default MemberCount;
