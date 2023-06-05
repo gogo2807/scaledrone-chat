98531465f5b74f8c4304fcc7609dcd4697a683a0
gogo2807 <60366714+gogo2807@users.noreply.github.com>
2023-06-05 12:54:29 +0200

    Dodan opis komponente.

--- src/components/MemberCount.js
+++ src/components/MemberCount.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import "../styles/MemberCount.css";
+
+/*ova komponenta prikazuje broj članova u sobi*/
 
 function MemberCount({ count }) {
   return <div className="members-count">{count} korisnika u sobi:</div>;
